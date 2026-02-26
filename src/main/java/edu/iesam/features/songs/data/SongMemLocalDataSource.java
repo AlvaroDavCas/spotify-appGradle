a3abbff9e7d16577fab4d3164c9de8cdad8582e5
alvar <alvarodaca@outlook.es>
2026-02-26 02:26:26 +0100

    Work in progress v5.1

--- src/main/java/edu/iesam/features/songs/data/SongMemLocalDataSource.java
+++ src/main/java/edu/iesam/features/songs/data/SongMemLocalDataSource.java
@@ -1,5 +1,6 @@
 package edu.iesam.features.songs.data;
 
+import edu.iesam.features.authors.domain.Author;
 import edu.iesam.features.songs.domain.Song;
 
 import java.util.ArrayList;
@@ -31,6 +32,10 @@ public class SongMemLocalDataSource {
             instance = new SongMemLocalDataSource();
         }
         return instance;
+    }
+
+    public void save(Song song) {
+        songsMemStorage.add(song);
     }
 
 }
